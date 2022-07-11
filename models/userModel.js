488a3f60e6e022091c81288fdd09b0d21318b6c8
kaushik1904 <kaushikprajapati1970@gmail.com>
2022-07-11 22:10:15 +0530

    modify password => select: false and and correctPassword funtion

--- models/userModel.js
+++ models/userModel.js
@@ -18,7 +18,8 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Please enter your password'],
-    minLength: 8
+    minLength: 8,
+    select:false
   },
   passwordConfirm: {
     type: String,
@@ -39,6 +40,10 @@ userSchema.pre('save',async function(next){
 
   this.passwordConfirm = undefined;
 });
+
+userSchema.methods.correctPassword = async function(candidatePassword,userPassword){
+  return await bcrypt.compare(candidatePassword,userPassword);
+}
 
 const User = mongoose.model('User', userSchema);
 
